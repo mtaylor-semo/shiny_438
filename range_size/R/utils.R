da125868e4ed4991c1bfd6faf6633b03b152609f
Michael S. Taylor (home) <mtaylor@semo.edu>
2021-11-05 14:05:11 -0500

    Enable req() for prediction tab. Code clean up.

--- range_size/R/utils.R
+++ range_size/R/utils.R
@@ -30,24 +30,12 @@ open_file <- function(tx, st = NULL) {
 #  read.csv(file_to_open, row.names = 1)
 }
 
-# open_state_file <- function(st, tx) {
-#   # State and taxon
-#   the_state <- str_replace_all(st, " ", "_") # For two word states
-#   file_to_open <- paste0("state_data/", the_state, "_", tx, ".csv")
-#   read.csv(file_to_open, row.names = 1)
-# }
-#
-# open_na_file <- function(natx) {
-#   # North American taxon
-#   file_to_open <- paste0("na_data/na_", natx, ".csv")
-#   read.csv(file_to_open, row.names = 1)
-# }
-#
+
 
 ## Prediction check. Move requirement check for predictions here.
 ## sn = student_name, ps = pred_state, pn = pred_na, pc = pred_ca
 pred_check <- function(sn = NULL, ps = NULL, pn = NULL, pc = NULL) {
-#  req(sn, ps, pn, pc)
+  req(sn, ps, pn, pc)
 }
 
 result_check <- function(exp = NULL)
