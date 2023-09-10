442d61f16ce992531072695d424c801180b2e74b
Michael S. Taylor <mtaylor@semo.edu>
2023-09-10 12:24:29 -0500

    Bit more lintr cleanup.

--- range_size/app.R
+++ range_size/app.R
@@ -8,7 +8,6 @@
 library(shiny)
 library(tibble)
 library(tidyr)
-#library(dplyr)
 library(stringr)
 library(ggplot2)
 
@@ -23,8 +22,8 @@ ui <- tagList(
       img(src = "semo_logo.png", height = "70px"),
       "Geographic range size"
     ),
-    # Instructions tab ------------------------------------------------------------
 
+    # Instructions tab --------------------------------------------------
     tabPanel(
       "Instructions",
       mainPanel(
@@ -52,7 +51,7 @@ ui <- tagList(
           hr(),
           p(),
           p("Enter your predictions at right, then press the
-               'Next' button."),
+            'Next' button."),
         ),
         column(
           6,
@@ -60,36 +59,46 @@ ui <- tagList(
           p("Will most species have small, moderate, or large range sizes?"),
           textAreaInput(
             inputId = "predict_na",
-            label = NULL, #"Enter your prediction:",
+            label = NULL,
             rows = 4,
-            placeholder = "North America prediction…"
-            ),
+            placeholder = "North America prediction…",
+            width = "90%"
+          ),
           hr(),
           p(strong("What do you predict for the state level?")),
           p("Will most species have small, moderate, or large range
             sizes?"),
           textAreaInput(
             inputId = "predict_state",
-            label = NULL, #"Enter your prediction:",
+            label = NULL,
             rows = 4,
-            placeholder = "State prediction…"
+            placeholder = "State level prediction…",
+            width = "90%"
           ),
           hr(),
           p(strong("Consider these five states:"), "(from south to
             north) Alabama, Tennessee, Kentucky, Illinois, Wisconsin."),
-          p("Do you think the range size of fishes will follow Rapoport's
-            Rule for range size from Alabama north to Wisconsin?"),
+          p("Do you think fishes, mussels, or crayfishes will follow
+            Rapoport's Rule for range size and species richness from 
+            Alabama north to Wisconsin?"),
+          p("Note: It is okay to say that all, some, or none will agree.
+            If you think only some taxa will follow Rapoport's Rule, be
+            sure to identify those taxa in your prediction."),
           textAreaInput(
             inputId = "predict_rapo_five",
-            label = NULL, #"Enter your prediction:",
+            label = NULL,
             rows = 4,
-            placeholder = "Rapoport's Rule prediction…"
+            placeholder = "Rapoport's Rule prediction…",
+            width = "90%"
           )
         ),
         column(
           3,
-          actionButton(inputId = "btn_next_pred", label = "Next", width = "35%"),
-          span(textOutput("prediction_error"), style = "color:#9D2235")
+          actionButton(inputId = "btn_next_pred",
+                       label = "Next",
+                       width = "35%"),
+          span(textOutput("prediction_error"),
+               style = "color:#9D2235")
         )
       )
     )
@@ -104,9 +113,9 @@ server <- function(input, output, session) {
 
   output$prediction_error <- renderText({
     if (input$student_name == "" |
-      input$predict_state == "" |
-      input$predict_na == "" |
-      input$predict_rapo_five == "") {
+        input$predict_state == "" |
+        input$predict_na == "" |
+        input$predict_rapo_five == "") {
       "Please fill in all blanks."
     }
   })
@@ -116,7 +125,7 @@ server <- function(input, output, session) {
       "Please interpret the histogram."
     }
   })
-  
+
   output$state_result_error <- renderText({
     if (input$state_result == "") {
       "Please interpret the histogram."
@@ -128,7 +137,7 @@ server <- function(input, output, session) {
       "Please interpret the bar charts."
     }
   })
-  
+
   ## Reactive values ---------------------------------------------------------
 
   state <- reactive({
@@ -145,14 +154,14 @@ server <- function(input, output, session) {
   spp_na <- reactive({
     open_file(tx = str_to_lower(input$na_taxon))
   })
-  
-  rapo <- reactive({
-    filter(rapo_data,
-    str_to_lower(input$rapo_taxon))
-  })
+
+  # rapo <- reactive({
+  #   filter(rapo_data,
+  #   str_to_lower(input$rapo_taxon))
+  # })
 
   plots <- reactiveValues(na = NULL, state = NULL, rapo = NULL)
-  
+
   results <- reactiveValues(na = NULL, state = NULL, ca = NULL)
 
 
@@ -160,16 +169,21 @@ server <- function(input, output, session) {
 
   observeEvent(input$btn_next_pred, {
     if (is.null(input$na_taxon)) {
-    # Comment out for development.
-     pred_check(sn = input$student_name,
-                ps = input$predict_state,
-                pn = input$predict_na,
-                pc = input$predict_rapo_five)
+      pred_check(
+        sn = input$student_name,
+        ps = input$predict_state,
+        pn = input$predict_na,
+        pc = input$predict_rapo_five
+      )
 
-    removeTab(inputId = "tabs", target = "Predictions")
-    appendTab(inputId = "tabs", tab = na_tab, select = TRUE)
+      removeTab(inputId = "tabs", target = "Predictions")
+      appendTab(inputId = "tabs",
+                tab = na_tab,
+                select = TRUE)
     } else {
-      showTab(inputId = "tabs", target = "North America", select = TRUE)
+      showTab(inputId = "tabs",
+              target = "North America",
+              select = TRUE)
     }
   })
 
@@ -185,21 +199,21 @@ server <- function(input, output, session) {
   observeEvent(input$btn_next_state, {
     if (is.null(input$ca_marine)) {
       result_check(exp = input$state_result)
-      appendTab(inputId = "tabs", tab = rapo_five_tab, select = TRUE)  
+      appendTab(inputId = "tabs", tab = rapo_five_tab, select = TRUE)
     } else {
-      showTab(inputId = "tabs", target = "Rapoport's Rule", select = TRUE) 
+      showTab(inputId = "tabs", target = "Rapoport's Rule", select = TRUE)
     }
   })
 
   observeEvent(input$btn_next_rapo_five, {
     if (is.null(input$summary)) {
       result_check(exp = input$rapo_five_result)
-      appendTab(inputId = "tabs", tab = summary_tab, select = TRUE)  
+      appendTab(inputId = "tabs", tab = summary_tab, select = TRUE)
     } else {
-      showTab(inputId = "tabs", target = "Summary", select = TRUE) 
+      showTab(inputId = "tabs", target = "Summary", select = TRUE)
     }
   })
-  
+
 
   ## Outputs -------------------------------------------------------------
 
@@ -215,29 +229,33 @@ server <- function(input, output, session) {
 
   output$state_numbers <- renderUI({
     dims <- dim(spp())
-    sprintf("%s has %d watersheds and %d species of %s.", input$state, dims[1], dims[2], str_to_lower(input$taxon))
+    sprintf("%s has %d watersheds and %d species of %s.",
+            input$state, dims[1], dims[2],
+            str_to_lower(input$taxon))
   })
 
   output$na_numbers <- renderUI({
     dims <- dim(spp_na())
-    sprintf("North America has %d watersheds and %d species of %s.", dims[1], dims[2], str_to_lower(input$na_taxon))
+    sprintf("North America has %d watersheds and %d species of %s.",
+            dims[1], dims[2],
+            str_to_lower(input$na_taxon))
   })
-  
+
   output$prediction_na <- renderUI({
     p("You predicted:")
     sprintf("%s", input$predict_na)
   })
-  
+
   output$prediction_state <- renderUI({
     p("You predicted:")
     sprintf("%s", input$predict_state)
   })
-  
+
   output$prediction_rapo_five <- renderUI({
     p("You predicted:")
     sprintf("%s", input$predict_rapo_five)
   })
-  
+
   output$rapo_five_info <- renderUI({
     p("Placeholder. How will I use this")
   })
@@ -247,29 +265,31 @@ server <- function(input, output, session) {
   ## State histograms ------------------------------------------------------
 
   output$state_histogram <- renderPlot({
-    numWatersheds <- colSums(spp())
-    numSpecies <- rowSums(spp())
+    num_watersheds <- colSums(spp())
+    num_species <- rowSums(spp())
 
     nws <- nrow(spp())
 
     bins <- input$bins
 
-    plots$state <- plotHistogram(dat = tibble(numWatersheds), x = numWatersheds, breaks = c(nws, 1))
+    plots$state <- plot_histogram(dat = tibble(num_watersheds),
+                                  x = num_watersheds,
+                                  breaks = c(nws, 1))
 
     plots$state
   }, res = res)
 
-  ## North America histogram -------------------------------------------------
+  ## North America histogram ------------------------------------------
 
   output$na_histogram <- renderPlot({
-    numWatersheds <- colSums(spp_na())
-    numSpecies <- rowSums(spp_na())
-
-    dat <- tibble(numWatersheds)
+    num_watersheds <- colSums(spp_na())
+    num_species <- rowSums(spp_na())
 
     nws <- nrow(spp_na()) # Number of watersheds for x-axis
 
-    plots$na <- plotHistogram(dat = tibble(numWatersheds), x = numWatersheds, breaks = c(nws, 5))
+    plots$na <- plot_histogram(dat = tibble(num_watersheds),
+                               x = num_watersheds,
+                               breaks = c(nws, 5))
 
     plots$na
   }, res = res)
@@ -281,32 +301,25 @@ server <- function(input, output, session) {
     data_to_plot <- filter(rapo_data,
                            taxon == str_to_lower(input$rapo_taxon))
     plots$rapo <- plot_rapo(plot_data = data_to_plot)
-    
+
     plots$rapo
-     }, res = res)
-  
-  
+  }, res = res)
+
+
   # Report Download ---------------------------------------------------------
-  
+
   # Report output idea from Shiny Gallery
   output$downloadReport <- downloadHandler(
     filename = function() {
-      stu_name <- str_to_lower(str_split(input$student_name, " ", simplify = TRUE))
-      # For the student (or famous soocer player) with only one name.
-      # What happens for students with three or more names?
+      stu_name <- str_to_lower(str_split(input$student_name,
+                                         " ",
+                                         simplify = TRUE))
       paste(
-        paste0(
-          rev(stu_name), 
-          collapse = "_"), 
-        "geographic_range.pdf", 
+        paste0(rev(stu_name),
+               collapse = "_"),
+        "geographic_range.pdf",
         sep = "_"
       )
-      # if (!is.na(stu_name[2])) {
-      #   paste(stu_name[2], stu_name[1], "geographic_range.pdf", sep = "_")  
-      # } else {
-      #   paste(stu_name[1], "geographic_range.pdf", sep = "_")
-      # }
-      
     },
     content = function(file) {
       notification_id <- showNotification(
@@ -323,9 +336,9 @@ server <- function(input, output, session) {
       on.exit(setwd(owd))
       file.copy(src, "range_report.Rmd", overwrite = TRUE)
       file.copy(src_tex, "tex_header.tex", overwrite = TRUE)
-      
+
       library(rmarkdown)
-      
+
       out <- render(
         "range_report.Rmd",
         pdf_document(latex_engine = "lualatex",
@@ -336,7 +349,7 @@ server <- function(input, output, session) {
       on.exit(removeNotification(notification_id), add = TRUE)
     }
   )
-  
+
 }
 
 # Run the application
