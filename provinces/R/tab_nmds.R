14e4e4dc647d55616e84cf150396b49c33aa5495
Michael S. Taylor <mtaylor@semo.edu>
2023-09-26 05:48:35 -0500

    Unique IDs between cluster and nmds tabs.

--- provinces/R/tab_nmds.R
+++ provinces/R/tab_nmds.R
@@ -8,7 +8,7 @@ nmds_tab <- tabPanel(
       3,
       wellPanel(
         selectInput(
-          inputId = "state_menu",
+          inputId = "state_menu_nmds",
           label = "Choose a state",
           choices = names(state_fishes),
           selected = "Montana",
@@ -16,30 +16,29 @@ nmds_tab <- tabPanel(
         ),
       ),
       actionButton(
-        inputId = "btn_next_cn",
+        inputId = "btn_next_nmds",
         label = "Next",
         width = "35%"
       ),
-      span(textOutput("pc_result_error"),
-           style = "color:#9D2235"
-      )
+      # span(textOutput("pc_result_error"),
+      #      style = "color:#9D2235"
+      # )
     ),
     column(
       9,
       plotOutput("nmds_plot",
                  width = "97%",
-                 height = "500px")
+                 height = "500px") #%>%
+        # withSpinner(type = 4,
+        #             color = semo_palette["cardiac_red"])
     ),
     fluidRow(
       column(
         6,
-        plotOutput("cluster_plot",
+        plotOutput("cluster_plot_rep",
                    width = "97%",
                    height = "400px"
-        ) %>%
-          withSpinner(type = 4,
-                      color = semo_palette["cardiac_red"])#,
-        # hr(),
+        ) #,
       ), # next column here
       column(
         width = 5,
@@ -47,7 +46,7 @@ nmds_tab <- tabPanel(
         do U.S. freshwater fishes follow Rapoport’s Rule for geographic
         range size? Explain."),
         textAreaInput(
-          inputId = "cluster_q4",
+          inputId = "nmds_q4",
           label = NULL,
           rows = 5,
           width = "97%"
