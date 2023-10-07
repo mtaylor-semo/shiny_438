206a378023a705f06a36a15f71d07b4b970072f3
Michael S. Taylor <mtaylor@semo.edu>
2023-10-07 09:59:03 -0500

    Rearrange overall layout. Prev-next buttons side-by-side.

--- provinces/R/tab_north_america.R
+++ provinces/R/tab_north_america.R
@@ -3,22 +3,26 @@
 na_richess_tab <- tabPanel(
   "North America",
   fluidRow(
-    column(
-      1,
-      prev_btn("btn_prev_na"),
-    ),
-    column(
-      2,
-      p("Scroll down for questions.")
-    ),
+    # column(
+    #   1,
+    #   prev_btn("btn_prev_na"),
+    # ),
+    # column(
+    #   2,
+    #   p("Scroll down for questions.")
+    # ),
     column(
       3,
       align = "right",
-      offset = 5,
+      offset = 7,
       span(
         textOutput("na_richness_result_error"),
         style = "color:#9D2235"
       )
+    ),
+    column(
+      1,
+      prev_btn("btn_prev_na"),
     ),
     column(
       1,
@@ -28,17 +32,29 @@ na_richess_tab <- tabPanel(
   hr(),
   fluidRow(
     column(
-      12,
+      3,
+      img(src = "relief_map.jpg", width = "97%"),
+      hr(),
+      p(
+        "Locations where species richness is very high are often called",
+        strong("biodiversity hotspots."), "Such hotspots are often areas of
+        conservation concern. Preserving biodiversity hotspots protects the
+        greatest number of species, with efficient use of conservation
+        dollars."
+      )
+    ),
+    column(
+      9,
       plotOutput(
         "na_richness_plot",
-        height = "600px"
+        height = "500px"
       ) %>%
         withSpinner(
           type = 4,
           color = semo_palette["cardiac_red"]
         ),
       br(),
-      p("Density plot of species richness for U.S. Freshwater fishes. Brighter
+      p("Species richness for U.S. Freshwater fishes. Brighter
         colors indicate greater species richness. Look carefully at the western
         half of the country. Notice that richness increases somewhat along the
         coast. Notice too that richness tends to follow the rivers.")
@@ -49,9 +65,7 @@ na_richess_tab <- tabPanel(
     column(
       6,
       p(strong("You predicted:")),
-      uiOutput("prediction_na_richness"),
-      hr(),
-      img(src = "relief_map.jpg", width = "97%")
+      uiOutput("prediction_na_richness")
     ),
     column(
       6,
@@ -62,17 +76,9 @@ na_richess_tab <- tabPanel(
         rows = nrows,
         width = "97%"
       ),
-      hr(),
-      p(
-        "Locations where species richness is very high are often called",
-        strong("biodiversity hotspots."), "Such hotspots are often areas of
-        conservation concern. Preserving biodiversity hotspots protects the
-        greatest number of species, with efficient use of conservation
-        dollars."
-      ),
-      hr(),
       p("Press the Next button above to make relief maps of different species
         groups.")
     )
   )
 )
+
