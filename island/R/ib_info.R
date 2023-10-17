acf5caf15cce6356361523dcfe4231f58e3753a3
Michael S. Taylor <mtaylor@semo.edu>
2023-10-17 07:08:38 -0500

    Final (or close) information for IB data sets.

--- island/R/ib_info.R
+++ island/R/ib_info.R
@@ -1,27 +1,43 @@
 herps_info <- tagList(
-  p("Classic early study of island biogeo."
+  p("This is a classic data set used to develop and support the species-area
+    relationship concept of island biogeography. Species richness is based on
+    the number of reptiles and amphibians found on seven islands."
+  ),
+  p("Data from Macarthur and Wilson 1967. The Theory of Island Biogeography. 
+    Monographs in Population Biology, No. 1. Princeton University Press, NJ."
   )
 )
   
 beetles_info <- tagList(
-  p("Beetles of the Florida Keys")
+  p("The beetles of this study are long-horned beetles (family Cerambycidae),
+    named for their very long antennae. At the time of this study, 91 species
+    of long-horned beetles were known from south Florida. The authors wanted
+    to know whether the distribution of the beetles in the Florida Keys fit
+    the species-area and species-distance relationship of island biogeography.
+    South Florida would be the primary source of species for the Florida Keys.
+    They sampled beetles from 11 islands. Distance was measured from a fixed
+    location in south Florida."
+  ),
+  p("Data from Browne and Peck 1996. The long-horned beetles of south Florida
+    (Cerambycidae: Coleoptera): biogeography and relationships with the Bahama
+    Islands and Cuba. Canadian Journal of Zoology. 74: 2154-2169."
+  )
 )
 
 mtn_info <- tagList(
-  p(
-    "Montaine mammals in this study were 15 mammal speices found on mountain
-    tops above
-    7500 m in the southwestern deserts of the United States. The mammals were
-    boreal mammals and thus adapted to cooler temperatures. The mammals could
-    not tolerate the warm temperatures in the valleys between the mountains. 
-    Thus, the mountain tops function as 'islands' in a valley 'sea.'"
+  p("Montaine mammals in this study were 15 mammal speices found on mountain
+    tops above 7500 m in the southwestern deserts of the United States. The
+    mammals were boreal mammals and thus adapted to cooler temperatures. The
+    mammals could not tolerate the warm temperatures in the valleys between
+    the mountains. Thus, the mountain tops function as “islands” in a valley
+    “sea.”"
   ),
   p("This study measured area, distance between mountain tops and also distance
     from smainlands (larger mountain ranges east and west of the study area). 
-    View species richness for all three 
-    variables. Do all three fit the island biogeography model? If not, which
-    do? Why do you think the others do not? Use the significance (p value) from
-    the regression table to guide your decisions." 
+    View species richness for all three variables. Do all three fit the island
+    biogeography model? If not, which do? Why do you think the others do not?
+    Use the significance (p value) from the regression table to guide your
+    decisions." 
   ),
   p("Data from Brown 1971. Mammals on mountaintops: nonequilibrium insular
     biogeography. The American Naturalist 105: 467-478."
@@ -29,22 +45,20 @@ mtn_info <- tagList(
 )
 
 arthro_info <- tagList(
-  p(
-    "Arboreal arthropods are insects, spiders, millipeds, isopods
-and related species that live in trees. The islands studied were 8 very small 
-mangrove islands off Sugarloaf Key in Florida. The goal was to determine how
-quickly islands returned to dynamic equilibrium between colonization and
-extinction. They censused the islands in 1969, reduced the size of the island
-by about 30-50%, censused again in 1970, reduced the size again by about 50%,
-and did a final census in 1971. Some islands could only be censused for two 
-years."
+  p("Arboreal arthropods are insects, spiders, millipeds, isopods and related
+    species that live in trees. The islands studied were 8 very small mangrove
+    islands off Sugarloaf Key in Florida. The mangroves were Red Mangrove trees.
+    The goal was to determine how quickly islands returned to dynamic
+    equilibrium between colonization and extinction. They censused the islands
+    in 1969, reduced the size of the island by about 30-50%, censused again
+    in 1970, reduced the size again by about 50%, and did a final census in
+    1971. Some islands could only be censused for two years."
   ),
-  p(
-    "View the results with an eye towards island size. How does richness change as
-island size decreases? After viewing the data by island, you can pick out
-the trends for each island when you view the entire data set at once."
+  p("View the results with an eye towards island size. How does richness change
+    as island size decreases? After viewing the data by island, you can pick out
+    the trends for each island when you view the entire data set at once."
   ),
-  p(
-    "Data from Simberloff 1976. Ecology 57: 629-648."
+  p("Data from Simberloff 1976. Experimental zoogeography of islands: effects
+    of island size. Ecology 57: 629-648."
   )
 )
