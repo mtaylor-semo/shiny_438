6558e82e2a729b2b2d78f0971fc52e095812ac16
Michael S. Taylor <mtaylor@semo.edu>
2023-10-24 08:17:25 -0500

    Rearrange layout. Cluster plot and watershed map side by side. Remove questions.

--- provinces/R/tab_cluster.R
+++ provinces/R/tab_cluster.R
@@ -42,40 +42,44 @@ cluster_tab <- tabPanel(
       ) %>%
         withSpinner(type = 4,
                     color = semo_palette["cardiac_red"]),
-      hr(),
-      uiOutput("watershed_map_cluster")
+      #hr(),
+      #uiOutput("watershed_map_cluster")
     ),
     column(
-      width = 6,
-      p("Based on this figure, combined with the previous graphs,
-        do U.S. freshwater fishes follow Rapoport’s Rule for geographic
-        range size? Explain."),
-      textAreaInput(
-        inputId = "cluster_question1",
-        label = NULL,
-        rows = 5,
-        width = "97%"
-      ),
-      hr(),
-      p("Considering the geographic ranges east of the Rocky Mountains,
-       what region of the U.S. seems to have most of the smaller bubbles?
-       Why do you think this is?"),
-      textAreaInput(
-        inputId = "cluster_question2",
-        label = NULL,
-        rows = 5,
-        width = "97%"
-      ),
-      hr(),
-      p("What geographic region west of the Rocky Mountains seems
-        to have the fewest bubbles? (Or, most of the smallest bubbles?)
-        Why do you think this is?"),
-      textAreaInput(
-        inputId = "cluster_question3",
-        label = NULL,
-        rows = 5,
-        width = "97%"
-      )
+      6,
+      uiOutput("watershed_map_cluster")
     )
+    # column(
+    #   width = 6,
+    #   p("Based on this figure, combined with the previous graphs,
+    #     do U.S. freshwater fishes follow Rapoport’s Rule for geographic
+    #     range size? Explain."),
+    #   textAreaInput(
+    #     inputId = "cluster_question1",
+    #     label = NULL,
+    #     rows = 5,
+    #     width = "97%"
+    #   ),
+    #   hr(),
+    #   p("Considering the geographic ranges east of the Rocky Mountains,
+    #    what region of the U.S. seems to have most of the smaller bubbles?
+    #    Why do you think this is?"),
+    #   textAreaInput(
+    #     inputId = "cluster_question2",
+    #     label = NULL,
+    #     rows = 5,
+    #     width = "97%"
+    #   ),
+    #   hr(),
+    #   p("What geographic region west of the Rocky Mountains seems
+    #     to have the fewest bubbles? (Or, most of the smallest bubbles?)
+    #     Why do you think this is?"),
+    #   textAreaInput(
+    #     inputId = "cluster_question3",
+    #     label = NULL,
+    #     rows = 5,
+    #     width = "97%"
+    #   )
+    # )
   )
 )
