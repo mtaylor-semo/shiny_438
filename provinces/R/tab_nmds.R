018e8030018ce0ca747f2b6456a8adccac90e925
Michael S. Taylor <mtaylor@semo.edu>
2023-10-24 11:37:29 -0500

    Reorganize interface with plot and watershed map side by side. Remove questions.

--- provinces/R/tab_nmds.R
+++ provinces/R/tab_nmds.R
@@ -24,7 +24,8 @@ nmds_tab <- tabPanel(
   hr(),
   fluidRow(
     column(
-      8,
+      7,
+      uiOutput("state_menu_nmds"),
       plotOutput(
         "nmds_plot",
         width = "97%",
@@ -33,11 +34,8 @@ nmds_tab <- tabPanel(
         withSpinner(
           type = 4,
           color = semo_palette["cardiac_red"]
-        )
-    ),
-    column(
-      4,
-      uiOutput("state_menu_nmds"),
+        ),
+      hr(),
       plotOutput(
         "cluster_plot_rep",
         width = "97%",
@@ -46,44 +44,48 @@ nmds_tab <- tabPanel(
       p("Choose the Cluster tab (or press the Prev button) to view a larger
         version of the cluster plot.")
     ),
-    hr(),
-    fluidRow(
-      column(
-        6,
-        uiOutput("watershed_map_nmds")
-      ),
-      column(
-        width = 6,
-        p("Based on this figure, combined with the previous graphs,
-        do U.S. freshwater fishes follow Rapoport’s Rule for geographic
-        range size? Explain."),
-        textAreaInput(
-          inputId = "nmds_question1",
-          label = NULL,
-          rows = nrows,
-          width = "97%"
-        ),
-        hr(),
-        p("Considering the geographic ranges east of the Rocky Mountains,
-       what region of the U.S. seems to have most of the smaller bubbles?
-       Why do you think this is?"),
-        textAreaInput(
-          inputId = "nmds_question2",
-          label = NULL,
-          rows = nrows,
-          width = "97%"
-        ),
-        hr(),
-        p("What geographic region west of the Rocky Mountains seems
-        to have the fewest bubbles? (Or, most of the smallest bubbles?)
-        Why do you think this is?"),
-        textAreaInput(
-          inputId = "nmds_question3",
-          label = NULL,
-          rows = nrows,
-          width = "97%"
-        ),
-      )
-    )
+    column(
+      5,
+      uiOutput("watershed_map_nmds")
+    ),
+    hr()
+    # fluidRow(
+    #   column(
+    #     6,
+    #     
+    #   ),
+    #   column(
+    #     width = 6,
+    #     p("Based on this figure, combined with the previous graphs,
+    #     do U.S. freshwater fishes follow Rapoport’s Rule for geographic
+    #     range size? Explain."),
+    #     textAreaInput(
+    #       inputId = "nmds_question1",
+    #       label = NULL,
+    #       rows = nrows,
+    #       width = "97%"
+    #     ),
+    #     hr(),
+    #     p("Considering the geographic ranges east of the Rocky Mountains,
+    #    what region of the U.S. seems to have most of the smaller bubbles?
+    #    Why do you think this is?"),
+    #     textAreaInput(
+    #       inputId = "nmds_question2",
+    #       label = NULL,
+    #       rows = nrows,
+    #       width = "97%"
+    #     ),
+    #     hr(),
+    #     p("What geographic region west of the Rocky Mountains seems
+    #     to have the fewest bubbles? (Or, most of the smallest bubbles?)
+    #     Why do you think this is?"),
+    #     textAreaInput(
+    #       inputId = "nmds_question3",
+    #       label = NULL,
+    #       rows = nrows,
+    #       width = "97%"
+    #     ),
+    #   )
+    # )
   )
 )
