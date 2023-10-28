76ffaaded4ddd57fa8596e8af9f2f3db95474abc
Michael S. Taylor <mtaylor@semo.edu>
2023-10-28 14:15:26 -0500

    Update the information, remove the question.

--- provinces/R/tab_north_america.R
+++ provinces/R/tab_north_america.R
@@ -55,31 +55,43 @@ na_richess_tab <- tabPanel(
           color = semo_palette["cardiac_red"]
         ),
       br(),
-      p("Species richness for U.S. Freshwater fishes. Brighter
-        colors indicate greater species richness. Look carefully at the western
-        half of the country. Notice that richness increases somewhat along the
-        coast. Notice too that richness tends to follow the rivers.")
-    )
-  ),
-  hr(),
-  fluidRow(
-    column(
-      6,
-      p(strong("You predicted:")),
-      uiOutput("prediction_na_richness")
-    ),
-    column(
-      6,
-      p(strong("Question:"), na_question1_text),
-      textAreaInput(
-        inputId = "na_question1",
-        label = NULL,
-        rows = nrows,
-        width = "97%"
-      ),
-      p("Press the Next button above to make relief maps of different species
-        groups.")
+      p("This plot shows species richness for 529 U.S. freshwater fishes.
+        Brighter colors indicate greater species richness. Look carefully at
+        the western half of the country. Notice that richness increases
+        somewhat along the coast. Notice too that richness tends to follow
+        the rivers."),
+      p("Reflect back on the “champaigne bubble plot” you made in the
+        Rapoport's Rule exercise. That exercise helped you understand the
+        distribution of species richness of freshwater fishes. Think about how
+        that plot compares to this map as you answer the questions on the
+        handout."),
+      p(strong("Important:"), "The range of colors that indicate species
+        richness varies based on the number of species in the data set.
+        In the plot above, the brightest colors may represent richness of
+        well over 100 species. On the next tab, the brightest colors
+        may represent 10 or 20 species. Your goal is simply to determine
+        which areas have the highest richness", em("for that data set."))
     )
   )
+  # hr(),
+  # fluidRow(
+  #   column(
+  #     6,
+  #     p(strong("You predicted:")),
+  #     uiOutput("prediction_na_richness")
+  #   ),
+  #   column(
+  #     6,
+  #     p(strong("Question:"), na_question1_text),
+  #     textAreaInput(
+  #       inputId = "na_question1",
+  #       label = NULL,
+  #       rows = nrows,
+  #       width = "97%"
+  #     ),
+  #     p("Press the Next button above to make relief maps of different species
+  #       groups.")
+  #   )
+  # )
 )
 
