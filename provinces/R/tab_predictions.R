c7a63f21304c9d93c6be146fd287e619a9a78674
Michael S. Taylor <mtaylor@semo.edu>
2023-10-28 13:52:35 -0500

    Overview text for Part 1.

--- provinces/R/tab_predictions.R
+++ provinces/R/tab_predictions.R
@@ -37,21 +37,34 @@ predictions_tab <- tabPanel(
     ),
     column(
       6,
-      p(strong("Think carefully about your prediction below."), "Is the United
-        States uniform from south to north? What about east to west? Think
-        about where the U.S. tends  to be wetter (more precipitation) or more
-        arid (drier). Do some regions have more rivers than other regions?
-        Will the U.S. have any regional climate effects due to rain shadows
-        or deserts? How might this influence the distribution of species
-        richness of freshwater fishes?"),
-      p(strong("What do you predict for species richness for U.S. freshwater
-               fishes?")),
-      p("Where in the U.S. do you think species richness will be the highest?
-        Northeastern U.S.? Southeastern U.S.? Northwest? Southwest? Midwest?
-        You may already an idea from the Rapoport's Rule exercises.
-        Take a look at the map of U.S. rivers that was given to you?  Below,
-        name the region and 3--4 rivers in that region where you think
-        diversity may be the highest. Explain your reasoning."),
+      h4("Part 1"),
+      
+       
+      p("In the Rapoport's Rule exercise, you gained a sense of how species
+        richness of freshwater fishes is distributed across the U.S. For this
+        exercise, you will explore the same data relative to the Pleistocene
+        glacial maximum and the fall line (see image at left). The area between
+        the glacial maximum and the fall line is called the", 
+        strong("interior highlands"), "consisting of the eastern highlands
+        (formally, the", tags$a(href = "https://en.wikipedia.org/wiki/Appalachian_Highlands", "Appalachian Highlands"), ") and the central highlands
+      (formally. the ", tags$a(href = "https://en.wikipedia.org/wiki/Ozarks", "Ozark Highlands"), ")."),
+      p("You will generate a “shaded relief map”, that uses colors to show
+        relative species richness, similar to the map at left. Your maps will
+        use dark colors to indicate low species richness 
+        (few species) and bright colors to indicate higher species richness.
+        After you explore overall species richness for the U.S. you will
+        explore the distribution of species richness for selected families 
+        of fishes."),
+      p(strong("Think carefully about your prediction."), "Where will species
+        richness be highest? Will it be north of the glacial maximum? Will
+        richness be highest in the interior highlands between the glacial 
+        maximum and coastal plain? Will richness be highest on the coastal 
+        plain? Why do you think so?"),
+      p("The data were obtained by creating a presence / absence matrix for each
+        species of native fish. Presence or absence was based on 1° x 1° 
+        longitude / latitude grids. All matrices were summed together to
+        calculate species richness for each 1° x 1° cell across the entire U.S.
+        The data set represents a total of 529 species")
       # hr(),
       # textAreaInput(
       #   inputId = "predict_na_richness",
