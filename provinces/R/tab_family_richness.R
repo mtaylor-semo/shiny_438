d5f3f020a2044152729a63e6a5321f2b0e62bef3
Michael S. Taylor <mtaylor@semo.edu>
2023-10-29 05:52:15 -0500

    Replace the question inputs with question prompts to guide their homework.

--- provinces/R/tab_family_richness.R
+++ provinces/R/tab_family_richness.R
@@ -4,15 +4,15 @@ family_richness_tab <- tabPanel(
   "Family Richness",
   fluidRow(
     column(
-      3,
-      align = "right",
-      offset = 7,
+      7,
+      align = "left",
       span(textOutput("family_richness_result_error"),
         style = "color:#9D2235"
       )
     ),
     column(
       1,
+      offset = 3,
       prev_btn("btn_prev_family_richness"),
     ),
     column(
@@ -50,24 +50,36 @@ family_richness_tab <- tabPanel(
   hr(),
   fluidRow(
     column(
-      width = 6,
-      p(strong("Question:"), family_richness_question1_text),
-      textAreaInput(
-        inputId = "family_richness_question1",
-        label = NULL,
-        rows = nrows,
-        width = "97%"
-      ),
-    ),
-    column(
-      6,
-      p(strong("Question:"), family_richness_question2_text),
-      textAreaInput(
-        inputId = "family_richness_question2",
-        label = NULL,
-        rows = nrows,
-        width = "97%"
-      )
+      width = 9,
+      offset = 3,
+      p("Carefully compare all of the families available in the drop-down menu.
+        Which families have the highest richness in the interior 
+        highlands (generally the eastern)? Which families have the
+        greatest richness outside of the highlands? Which family has the 
+        greatest richness on the coastal plain? Which families have the
+        greatest richness in the Great Lakes or northwest? Read the descriptions
+        for these two families. What do they have in common? How does that fit
+        with the richness shown on their maps?")
     )
+  #   column(
+  #     width = 6,
+  #     p(strong("Question:"), family_richness_question1_text),
+  #     textAreaInput(
+  #       inputId = "family_richness_question1",
+  #       label = NULL,
+  #       rows = nrows,
+  #       width = "97%"
+  #     ),
+  #   ),
+  #   column(
+  #     6,
+  #     p(strong("Question:"), family_richness_question2_text),
+  #     textAreaInput(
+  #       inputId = "family_richness_question2",
+  #       label = NULL,
+  #       rows = nrows,
+  #       width = "97%"
+  #     )
+  #   )
   )
 )
